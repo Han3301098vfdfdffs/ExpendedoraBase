6e3d65f63171175c50809e2070d919279ed903c7
ALUMNO <ALUMNO@L28-2543305>
2022-08-24 12:03:48 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ExpendedoraBase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Expendedora miExpendedora = new Expendedora();
+            Console.WriteLine();
+        }
+    }
+}
